66c97d3d2984f53c1e164eb6dc1aa9f407cd988b
StevenHsu331 <steven50410@gmail.com>
2023-02-07 20:26:48 +0800

    Initial Commit

---
+++ Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by 許景評 on 2023/2/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by 許景評 on 2023/2/7.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
