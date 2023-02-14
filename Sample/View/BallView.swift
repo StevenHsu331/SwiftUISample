972f418a1ba4cc604919931432172479174c09ac
StevenHsu331 <steven50410@gmail.com>
2023-02-14 16:47:11 +0800

    init structure

---
+++ Sample/View/BallView.swift
@@ -0,0 +1,20 @@
+//
+//  BallView.swift
+//  Sample
+//
+//  Created by 許景評 on 2023/2/14.
+//
+
+import SwiftUI
+
+struct BallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BallView_Previews: PreviewProvider {
+    static var previews: some View {
+        BallView()
+    }
+}
